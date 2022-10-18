3b2f30157327994a7d19890f13967bab1d5b8df8
devsohyun <iamsohyunjun@gmail.com>
2022-10-18 18:44:51 +0900

    Getting float to apply

--- render/Assets/_App/Scripts/Managers/OSCManager.cs
+++ render/Assets/_App/Scripts/Managers/OSCManager.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReceiveOSC : MonoBehaviour
+public class OSCManager : MonoBehaviour
 {
     public OSC osc;
 
-    // public class Coordinate
-    // {
-    //     public float latitude;
-    //     public float longitude;
-    // }
+    public float latitude;
+    public float longitude;
     
     // Start is called before the first frame update
     void Start()
@@ -30,8 +27,8 @@ public class ReceiveOSC : MonoBehaviour
 
         // string coordinate = JsonUtility.FromJson<Coordinate>("{\"coordinate\":" + message.values[0].ToString() + "}");
 
-        float latitude = float.Parse(message.values[0].ToString());
-        float longitude = float.Parse(message.values[1].ToString());
+        latitude = float.Parse(message.values[0].ToString());
+        longitude = float.Parse(message.values[1].ToString());
         Debug.Log("latitude: " + latitude + ", longitude: " + longitude);
 
     }
